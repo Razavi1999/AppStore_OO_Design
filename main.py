8212d56269870b01f9ad65a4a8636208d734a48c
mrm78 <mr.mirshamsi.78@gmail.com>
2023-01-24 12:22:28 +0330

    rm unusable files

--- main.py
+++
@@ -1,9 +0,0 @@
-from home_page import HomePage
-from UserController import UserController
-from SearchResultView import SearchResultView
-
-applications = []
-SRV = SearchResultView(applications)
-hm = HomePage(SRV)
-
-user_controller = UserController()
